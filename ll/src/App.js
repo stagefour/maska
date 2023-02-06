05be20f33afb7c3d2e81f9ab18463a4627eb6d90
Pawel Banasiewicz <xghoul@gmail.com>
2023-02-06 07:40:31 +0100

    budujemy aplikacje

--- ll/src/App.js
+++ ll/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './assets/Header'
+import Nav from './assets/Nav'
+import Main from './assets/Main'
+import Footer from './assets/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    <Footer/>
+    </>
   );
 }
 
